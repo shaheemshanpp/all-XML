bce3baa288ea3f26aba694b230771aec41fcd380
Shaheem PP <shanofficial2000@gmail.com>
2020-07-21 06:53:53 +0530

    Commit 21/07/2020 6:30AM

---
+++ app/src/main/java/com/example/questionpaper/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.questionpaper
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        classsroomhero.setOnClickListener {
+            val intent = Intent(this, clasroomheroku::class.java)
+            startActivity(intent)
+        }
+        errorBtn.setOnClickListener {
+            val intent = Intent(this, error_page_activity::class.java)
+            startActivity(intent)
+        }
+        herologin.setOnClickListener {
+            val intent = Intent(this, herokulogin::class.java)
+            startActivity(intent)
+        }
+        timetable.setOnClickListener {
+            val intent = Intent(this, com.example.questionpaper.timetable::class.java)
+            startActivity(intent)
+        }
+    }
+}
